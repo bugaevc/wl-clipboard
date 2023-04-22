bugaevc/wl-clipboard

commit d66fb03313128544b36419e74578cc3fb8663aef

Move popup surface handling to types/popup-surface

src/types/popup-surface.c
@@ -1,0 +1,95 @@
+/* wl-clipboard
+ *
+ * Copyright © 2019 Sergey Bugaev <bugaevc@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "types/popup-surface.h"
+#include "types/shell.h"
+#include "types/shell-surface.h"
+#include "util/files.h"
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void popup_surface_init(struct popup_surface *self) {
+    self->wl_surface = wl_compositor_create_surface(self->wl_compositor);
+    self->shell_surface = shell_create_shell_surface(
+        self->shell,
+        self->wl_surface
+    );
+
+    /* Signal that the surface is ready to be configured */
+    wl_surface_commit(self->wl_surface);
+    wl_display_roundtrip(self->wl_display);
+
+    if (self->wl_surface == NULL) {
+        /* It's possible that we were given focus
+         * (without ever commiting a buffer) during
+         * the above roundtrip, in which case the
+         * handlers may have already destroyed the
+         * surface. No need to do anything further in
+         * that case.
+         */
+        free(self);
+        return;
+    }
+
+    /* Remember that after this point, we should
+     * free() self when it gets destroyed.
+     */
+    self->should_free_self = 1;
+
+    int width = 1;
+    int height = 1;
+    int stride = width * 4;
+    int size = stride * height;
+
+    /* Open an anonymous file and write some zero bytes to it */
+    int fd = create_anonymous_file();
+    ftruncate(fd, size);
+
+    /* Create a shared memory pool */
+    struct wl_shm *wl_shm = self->wl_shm;
+    struct wl_shm_pool *wl_shm_pool = wl_shm_create_pool(wl_shm, fd, size);
+
+    /* Allocate the buffer in that pool */
+    struct wl_buffer *wl_buffer = wl_shm_pool_create_buffer(
+        wl_shm_pool,
+        0,
+        width,
+        height,
+        stride,
+        WL_SHM_FORMAT_ARGB8888
+    );
+    /* We're using ARGB, so zero bytes mean
+     * a fully transparent pixel, which happens
+     * to be exactly what we want.
+     */
+
+    wl_surface_attach(self->wl_surface, wl_buffer, 0, 0);
+    wl_surface_damage(self->wl_surface, 0, 0, width, height);
+    wl_surface_commit(self->wl_surface);
+}
+
+void popup_surface_destroy(struct popup_surface *self) {
+    shell_surface_destroy(self->shell_surface);
+    wl_surface_destroy(self->wl_surface);
+
+    if (self->should_free_self) {
+        free(self);
+    }
+}

src/boilerplate.c
@@ -20,9 +20,10 @@
 #include "types/keyboard.h"
 #include "types/shell-surface.h"
 #include "types/shell.h"
+#include "types/popup-surface.h"
 
-static struct shell_surface *shell_surface = NULL;
 static struct shell *shell = NULL;
+static struct popup_surface *popup_surface = NULL;
 
 static void process_new_seat(struct wl_seat *new_seat);
 
@@ -293,52 +294,19 @@ void popup_tiny_invisible_surface() {
      */
     wl_display_dispatch(display);
 
-    surface = wl_compositor_create_surface(compositor);
-    shell_surface = shell_create_shell_surface(shell, surface);
-    /* Signal that the surface is ready to be configured */
-    wl_surface_commit(surface);
-    /* Wait for the surface to be configured */
-    wl_display_roundtrip(display);
+    popup_surface = calloc(1, sizeof(struct popup_surface));
+    popup_surface->wl_display = display;
+    popup_surface->wl_compositor = compositor;
+    popup_surface->wl_shm = shm;
+    popup_surface->shell = shell;
 
-    if (surface == NULL) {
-        /* It's possible that we've been given focus without us
-         * ever commiting a buffer, in which case the handlers
-         * may have already destroyed the surface; there's no
-         * way or need for us to commit a buffer in that case.
-         */
-        return;
-    }
-
-    int width = 1;
-    int height = 1;
-    int stride = width * 4;
-    int size = stride * height;  // bytes
-
-    /* Open an anonymous file and write some zero bytes to it */
-    int fd = create_anonymous_file();
-    ftruncate(fd, size);
-
-    /* Turn it into a shared memory pool */
-    struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, size);
-
-    /* Allocate the buffer in that pool */
-    struct wl_buffer *buffer = wl_shm_pool_create_buffer(pool,
-        0, width, height, stride, WL_SHM_FORMAT_ARGB8888);
-    /* Zeros in ARGB8888 mean fully transparent */
-
-    wl_surface_attach(surface, buffer, 0, 0);
-    wl_surface_damage(surface, 0, 0, width, height);
-    wl_surface_commit(surface);
+    popup_surface_init(popup_surface);
 }
 
 void destroy_popup_surface() {
-    if (shell_surface != NULL) {
-        shell_surface_destroy(shell_surface);
-        shell_surface = NULL;
-    }
-    if (surface != NULL) {
-        wl_surface_destroy(surface);
-        surface = NULL;
+    if (popup_surface != NULL) {
+        popup_surface_destroy(popup_surface);
+        popup_surface = NULL;
     }
 }
 

src/boilerplate.h
@@ -46,7 +46,6 @@ struct wl_seat *seat;
 struct wl_compositor *compositor;
 struct wl_shm *shm;
 struct wl_shell *wl_shell;
-struct wl_surface *surface;
 
 #ifdef HAVE_XDG_SHELL
 struct xdg_wm_base *xdg_wm_base;

src/meson.build
@@ -114,6 +114,8 @@ lib = static_library(
         'types/shell.c',
         'types/shell-surface.h',
         'types/shell-surface.c',
+        'types/popup-surface.h',
+        'types/popup-surface.c',
     ],
     dependencies: wayland,
     link_with: protocol_deps

src/types/popup-surface.h
@@ -1,0 +1,43 @@
+/* wl-clipboard
+ *
+ * Copyright © 2019 Sergey Bugaev <bugaevc@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TYPES_POPUP_SURFACE_H
+#define TYPES_POPUP_SURFACE_H
+
+#include <wayland-client.h>
+
+struct shell;
+struct shell_surface;
+
+struct popup_surface {
+    /* These fields are initialized by the creator */
+    struct wl_display *wl_display;
+    struct wl_compositor *wl_compositor;
+    struct wl_shm *wl_shm;
+    struct shell *shell;
+
+    /* These fields are initialized by the implementation */
+    struct shell_surface *shell_surface;
+    struct wl_surface *wl_surface;
+    int should_free_self;
+};
+
+void popup_surface_init(struct popup_surface *self);
+void popup_surface_destroy(struct popup_surface *self);
+
+#endif /* TYPES_POPUP_SURFACE_H */

commit 1270e06366fbf53d18de3ae22402418574931978

Fix a reentrancy-related regression
It is possible that the compositor gives our popup surface focus before
we ever commit a buffer. In that case, we run the rest of our logic
immediately, and popup_surface_init() knows not to proceed with creating
a buffer. However, that has regressed in commit
d66fb03313128544b36419e74578cc3fb8663aef, where we started setting
popup_surface to NULL, but still checking for wl_surface being NULL to
detect that case. Fix this by also resetting self->wl_surface to NULL.

Fixes https://github.com/bugaevc/wl-clipboard/issues/174

src/types/popup-surface.c
@@ -183,6 +183,8 @@ void popup_surface_destroy(struct popup_surface *self) {
     }
 #endif
     wl_surface_destroy(self->wl_surface);
+    /* Let popup_surface_init() know we're already done */
+    self->wl_surface = NULL;
     free(self->shell);
 
     if (self->should_free_self) {